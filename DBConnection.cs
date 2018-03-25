c54d4a13dfb1fbdad2c0fe660d3ab6304da60b8e
CHINTARAM <31130329+CHINTARAM@users.noreply.github.com>
2018-03-25 12:13:23 +0530

    app_code

---
+++ DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+/// <summary>
+/// Summary description for DBConnection
+/// </summary>
+public class DBConnection
+{
+    public static SqlConnection objConn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString);
+    public DBConnection()
+    {
+
+    }
+}
+public static class GlobalValue
+{
+    public static double _feedtrustworth;
+
+    public static double _appreciation;
+
+    public static double _UserTrustDegree;
+
+}
